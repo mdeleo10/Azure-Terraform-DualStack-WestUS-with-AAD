b6883fce5057ea9c34a42e52b8d0a7d295b39f04
Michael De Leo <mdeleo10@hotmail.com>
2023-05-20 23:04:35 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,14 @@
+variable "resource_group_name_prefix" {
+  default       = "rg-DualStack-West"
+  description   = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
 
+variable "resource_group_location" {
+  default       = "westus"
+  description   = "Location of the resource group."
+}
+
+variable "admin_username" {
+  default       = "mdeleo"
+  description   = "Administrator Username"
+}
