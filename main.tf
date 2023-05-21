58c9b609a3e129bcd925501e82ea0713ab169701
Michael De Leo <mdeleo10@hotmail.com>
2023-05-21 15:38:48 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,4 @@
 terraform {
-  required_providers {
-    azurerm = {
-        source  = "hashicorp/azurerm"
-        version = "=3.0.0"
-    }
-  }
   backend "azurerm" {
     resource_group_name     = "rg-terraform-state-001"
     storage_account_name    = "cloudmdterraformstate"
