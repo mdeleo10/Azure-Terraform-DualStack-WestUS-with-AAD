8dcd7a69e51736eb82899ef68ffef44518524051
Michael De Leo <mdeleo10@hotmail.com>
2023-05-21 15:37:13 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "=3.0.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name     = "rg-terraform-state-001"
+    storage_account_name    = "cloudmdterraformstate"
+    container_name          = "tfstate"
+    key                     = "tfstate"
+  }
+}
 
 
 # Generate random password
