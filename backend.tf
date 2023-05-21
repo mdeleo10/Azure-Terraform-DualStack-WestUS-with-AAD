1463d5768ab0f2fd78050e454642654ea27189b7
Michael De Leo <mdeleo10@hotmail.com>
2023-05-21 15:34:47 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "azurerm" {
     resource_group_name     = "rg-terraform-state-001"
-    storage_account_name    = "cloudninjaterraformstate"
+    storage_account_name    = "cloudmdterraformstate"
     container_name          = "tfstate"
     key                     = "tfstate"
   }
