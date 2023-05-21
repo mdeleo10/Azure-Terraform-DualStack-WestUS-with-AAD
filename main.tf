aeedc10270f6c6df4a0258d6172e8bec3fb0dc52
Michael De Leo <mdeleo10@hotmail.com>
2023-05-20 22:18:11 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+backend "azurerm" {
+  resource_group_name  = "tamopstfstates"
+  storage_account_name = "tfstatedevops"
+  container_name       = "terraformgithubexample"
+  key                  = "terraformgithubexample.tfstate"
+}
+
 # Generate random password
 resource "random_password" "linux-vm-password" {
   length           = 16
